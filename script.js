cde1ba13913c1c9d878c97fcea482a29581f7f50
Michael Fursa <bellamy9678@yahoo.com>
2020-03-12 23:00:53 +0300

    feat: add portfolio tabs

--- script.js
+++ script.js
@@ -8,8 +8,38 @@ window.onload = function() {
                 MENU[j].classList.remove('active');
             }
             MENU[i].classList.add('active');
-            const SECTION = document.querySelectorAll('section');
         }
     }
 
+    const TAGS = document.querySelector('.tags-group__list').children;
+    
+    function mixPictures() {
+        const PICTURES = document.querySelector('.portfolio__examples').children;
+        let arr = [];
+        for (let i = 0; i < PICTURES.length; i++) {
+            arr.push(PICTURES[i]);
+        }
+        arr.sort((a, b) => {
+            return 0.5 - Math.random();
+        });
+        return arr;
+    }
+
+    for (let i = 0; i < TAGS.length; i++) {
+        TAGS[i].onclick = () => {
+            for (let j = 0; j < TAGS.length; j++) {
+                TAGS[j].classList.remove('active-tag');
+            }
+            TAGS[i].classList.add('active-tag');
+            let mixedPictureArr = mixPictures();
+            let output = document.createDocumentFragment();
+            mixedPictureArr.forEach(function(elem) {
+                let img = document.createElement('img');
+                img = elem;
+                output.appendChild(img);
+            });
+            const CONTAINER = document.querySelector(".portfolio__examples");
+            CONTAINER.appendChild(output);
+        }
+    }
 }
