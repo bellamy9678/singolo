8b07c84440854ef708dadea6cea621d59198bbb8
Michael Fursa <bellamy9678@yahoo.com>
2020-03-12 22:07:51 +0300

    feat: add js-header-menu

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert('hi');
+window.onload = function() {
+    
+    const MENU = document.querySelector('.options__menu').children;
+
+    for (let i = 0; i < MENU.length; i++) {
+        MENU[i].onclick = () => {
+            for (let j = 0; j < MENU.length; j++) {
+                MENU[j].classList.remove('active');
+            }
+            MENU[i].classList.add('active');
+            const SECTION = document.querySelectorAll('section');
+        }
+    }
+
+}
